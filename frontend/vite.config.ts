4562ee763d492824d9977fea262931a449f843e9
quanglv <quanglvhust@gmail.com>
2026-07-30 23:21:57 +0700

    Update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 5555,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://localhost:9999',
         changeOrigin: true,
       },
     },
